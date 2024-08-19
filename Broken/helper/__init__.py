49c017d1fb108c8ea716b89b6349b3563fd0401a
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-08-19 16:22:51 +0530

    Add files via upload

--- Broken/helper/__init__.py
+++ Broken/helper/__init__.py
@@ -1,1 +1,14 @@
+import os
+import sys
+from pyrogram import Client
 
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Broken"])
+
+async def join(client):
+    try:
+        await client.join_chat("brokenxNetwork")
+    except BaseException:
+        pass
